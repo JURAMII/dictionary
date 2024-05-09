92ff6985b1377c10ca6a1175841f61cd18316bc4
Danbi <4679jeong@naver.com>
2024-05-09 13:02:32 +0900

    6

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"/dictionary/",
+
   plugins: [react()],
   // server:{
   //   port:3030,
